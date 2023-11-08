6ff8b928a5d5f542f43959db6825dfc91220b18f
Arima1213 <arima3992@gmail.com>
2023-11-08 19:45:40 +0700

    memindahkan seeder produk ke produkSeeder dan menambahkan 4 produk lainnya

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Product;
 use Illuminate\Support\Str;
-use App\Models\DetailProduct;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use App\Models\DetailGambarProduct;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+        $this->call(ProductSeeder::class);
 
         DB::table('users')->insert([
             'nama' => 'ekikkristiawan',
@@ -55,122 +52,6 @@ class DatabaseSeeder extends Seeder
             'gender' => 'Lainnya',
             'role' => 'user',
             'remember_token' => Str::random(10),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
-
-        // Buat produk pertama
-        $product1 = Product::create([
-            'nama_product' => 'Baju Gaun Elegan',
-            'stock' => 20,
-            'rating' => '4.5',
-            'harga' => 500000,
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
-        // Detail produk untuk produk pertama
-        DetailProduct::create([
-            'id_product' => $product1->id,
-            'deskripsi' => 'Baju gaun elegan dengan motif bunga, cocok untuk acara formal.',
-            'rating' => 4,
-            'merk' => 'FashionElegance',
-            'motif' => 'Bunga',
-            'panjang_kain' => 150,
-            'seller' => 'Toko Fashion Elegance',
-            'bahan' => 'Satin',
-            'size' => 'M',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
-        // Gambar produk untuk produk pertama
-        DetailGambarProduct::create([
-            'id_product' => $product1->id,
-            'gambar' => 'https://source.unsplash.com/800x800/?fashion,woman,dress',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-        // Gambar produk untuk produk pertama
-        DetailGambarProduct::create([
-            'id_product' => $product1->id,
-            'gambar' => 'https://source.unsplash.com/800x800/?fashion,woman,dress',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-        // Gambar produk untuk produk pertama
-        DetailGambarProduct::create([
-            'id_product' => $product1->id,
-            'gambar' => 'https://source.unsplash.com/800x800/?fashion,woman,dress',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-        // Gambar produk untuk produk pertama
-        DetailGambarProduct::create([
-            'id_product' => $product1->id,
-            'gambar' => 'https://source.unsplash.com/800x800/?fashion,woman,dress',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
-        // Buat produk kedua
-        $product2 = Product::create([
-            'nama_product' => 'Kemeja Klasik Pria',
-            'stock' => 15,
-            'rating' => '4.7',
-            'harga' => 250000,
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
-        // Detail produk untuk produk kedua
-        DetailProduct::create([
-            'id_product' => $product2->id,
-            'deskripsi' => 'Kemeja klasik pria dengan desain minimalis dan elegan.',
-            'rating' => 4,
-            'merk' => 'MenStyle',
-            'motif' => 'Solid',
-            'panjang_kain' => 160,
-            'seller' => 'Toko MenStyle',
-            'bahan' => 'Katun',
-            'size' => 'L',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-
-        // Gambar produk untuk produk kedua
-        DetailGambarProduct::create([
-            'id_product' => $product2->id,
-            'gambar' => 'https://source.unsplash.com/800x800/?fashion,man,shirt',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-        // Gambar produk untuk produk kedua
-        DetailGambarProduct::create([
-            'id_product' => $product2->id,
-            'gambar' => 'https://source.unsplash.com/800x800/?fashion,man,shirt',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-        // Gambar produk untuk produk kedua
-        DetailGambarProduct::create([
-            'id_product' => $product2->id,
-            'gambar' => 'https://source.unsplash.com/800x800/?fashion,man,shirt',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-        // Gambar produk untuk produk kedua
-        DetailGambarProduct::create([
-            'id_product' => $product2->id,
-            'gambar' => 'https://source.unsplash.com/800x800/?fashion,man,shirt',
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
-        // Gambar produk untuk produk kedua
-        DetailGambarProduct::create([
-            'id_product' => $product2->id,
-            'gambar' => 'https://source.unsplash.com/800x800/?fashion,man,shirt',
             'created_at' => now(),
             'updated_at' => now()
         ]);
