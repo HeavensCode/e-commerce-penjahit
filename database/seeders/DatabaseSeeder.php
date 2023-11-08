a654a2611f80bb1d51bb6a5182c638fe0f8d3174
Arima1213 <arima3992@gmail.com>
2023-11-08 20:59:16 +0700

    fix tampilan produk, migration serta seeder dan views

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(TokoSeeder::class);
         $this->call(ProductSeeder::class);
 
         DB::table('users')->insert([
